f4d858dd90b2996ca04e904580ab2c32ae367f8e
kartikwar <kartiksirwani@gmail.com>
2022-08-07 11:57:33 +0530

    remove element from array inplace

---
+++ lists/two_pointers/remove_element.py
@@ -0,0 +1,29 @@
+'''Problem Link:- https://www.interviewbit.com/problems/remove-element-from-array/'''
+
+class Solution:
+	# @param A : list of integers
+	# @param B : integer
+	# @return an integer
+	def removeElement(self, A, B):
+		matching = 0
+		n = len(A)
+		if n == 0:
+			return A
+		for i in range(n):
+			if A[i] == B:
+				matching += 1
+			elif matching > 0:
+				A[i-matching] = A[i]
+		A = A[:n-matching]
+		return len(A)
+
+
+if __name__ == '__main__':
+	# A = [ 2, 0, 1, 2, 0, 3, 2, 2, 2, 1, 0, 0, 0, 1, 0, 0, 2, 2, 2, 3, 2, 3, 1, 2, 1, 2, 2, 3, 2, 3, 0, 3, 0, 2, 1, 2, 0, 0, 3, 2, 3, 0, 3, 0, 2, 3, 2, 2, 3, 1, 3, 3, 0, 3, 3, 0, 3, 3, 2, 0, 0, 0, 0, 1, 3, 0, 3, 1, 3, 1, 0, 2, 3, 3, 3, 2, 3, 3, 2, 2, 3, 3, 3, 1, 3, 2, 1, 0, 0, 0, 1, 0, 3, 2, 1, 0, 2, 3, 0, 2, 1, 1, 3, 2, 0, 1, 1, 3, 3, 0, 1, 2, 1, 2, 2, 3, 1, 1, 3, 0, 2, 2, 2, 2, 1, 0, 2, 2, 2, 1, 3, 1, 3, 1, 1, 0, 2, 2, 0, 2, 3, 0, 1, 2, 1, 1, 3, 0, 2, 3, 2, 3, 2, 0, 2, 2, 3, 2, 2, 0, 2, 1, 3, 0, 2, 0, 2, 1, 3, 1, 1, 0, 0, 3, 0, 1, 2, 2, 1, 2, 0, 1, 0, 0, 0, 1, 1, 0, 3, 2, 3, 0, 1, 3, 0, 0, 1, 0, 1, 0, 0, 0, 0, 3, 2, 2, 0, 0, 1, 2, 0, 3, 0, 3, 3, 3, 0, 3, 3, 1, 0, 1, 2, 1, 0, 0, 2, 3, 1, 1, 3, 2 ]
+	# A = [4, 1, 1, 2, 1, 3]
+	A = [4,1,1,1]
+	# B = 2
+	B = 1
+
+	sol = Solution()
+	print(sol.removeElement(A, B))
